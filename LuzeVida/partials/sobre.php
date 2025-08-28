ecc134d233a9b71baab472b462779e54a6b1e966
Antony Nelson <contato.aninova@gmail.com>
2025-08-28 16:16:29 -0300

    Delete LuzeVida directory

--- LuzeVida/partials/sobre.php
+++
@@ -1,5 +0,0 @@
-<div class="sobre-residencial">
-    <h2>Sobre Residencial!</h2>
-    <p>Somos uma casa de repouso comprometida com o bem-estar e a qualidade de vida dos nossos idosos. Nossa missão é proporcionar um ambiente acolhedor e seguro, oferecendo cuidados especializados para garantir que cada residente tenha o suporte necessário para viver com dignidade, conforto e alegria.</p>
-    <a href="quemsomos.php" class="botao-s" alt="Saiba mais sobre nós" title="Saiba mais sobre nós">Saiba mais</a>
-</div>
